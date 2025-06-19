df72c88f43c5ab2e6b2a90f7af26af0e0f634e9d
ck314 <peter@hatchcoding.com>
2025-06-19 15:13:50 -0400

    More changes for Github Pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/AlphaMole-v2/',
+  build: {
+    outDir: 'docs'
+  },
   plugins: [react()],
 })
